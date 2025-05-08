2b29421f7dd358659123c63f61dcbb630cefd5ad
mytcgd <131883869+mytcgd@users.noreply.github.com>
2025-05-09 04:43:34 +0800

    Update up_v.sh

--- my/x/up_v.sh
+++ my/x/up_v.sh
@@ -28,25 +28,28 @@ if source /root/env.yml; then
   fi
 
   if [ -n "$V_PORT" ]; then
-    if [ -n "$XHTTP_PATH" ]; then
-      vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#${country_abbreviation}-${SUB_NAME}"
-      xhttp_url="vless://${UUID}@${CF_IP}:${CFPORT}?encryption=none&security=tls&sni=${ARGO_DOMAIN}&type=xhttp&host=${ARGO_DOMAIN}&path=%2F${XHTTP_PATH}%3Fed%3D2048&mode=packet-up#${country_abbreviation}-${SUB_NAME}-xhttp"
-      UPLOAD_DATA="$vless_url\n$xhttp_url"
-    else
-      # VMESS="{ \"v\": \"2\", \"ps\": \"${country_abbreviation}-${SUB_NAME}\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/${VMESS_WSPATH}?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\" }"
-      # vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
-      vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#${country_abbreviation}-${SUB_NAME}"
-      # UPLOAD_DATA="$vmess_url\n$vless_url"
-      UPLOAD_DATA="$vless_url"
+    if [ -n "${VLESS_WSPATH}" ] && [ -z "${XHTTP_PATH}" ]; then
+    vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#${country_abbreviation}-${SUB_NAME}"
+    UPLOAD_DATA="$vless_url"
+    fi
+    if [ -n "${XHTTP_PATH}" ] && [ -z "${VLESS_WSPATH}" ]; then
+    xhttp_url="vless://${UUID}@${CF_IP}:${CFPORT}?encryption=none&security=tls&sni=${ARGO_DOMAIN}&type=xhttp&host=${ARGO_DOMAIN}&path=%2F${XHTTP_PATH}%3Fed%3D2048&mode=packet-up#${country_abbreviation}-${SUB_NAME}-xhttp"
+    UPLOAD_DATA="$xhttp_url"
     fi
   fi
 
   if [ -n "$REAL_PORT" ] && [ -n "$shortid" ]; then
-    realitytcp_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&sid=${shortid}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}-realtcp"
-    UPLOAD_DATA="$UPLOAD_DATA\n$realitytcp_url"
+    reality_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&sid=${shortid}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}-realtcp"
+    UPLOAD_DATA="$UPLOAD_DATA\n$reality_url"
   elif [ -n "$REAL_PORT" ] && [ -z "$shortid" ]; then
-    realitytcp_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}-realtcp"
-    UPLOAD_DATA="$UPLOAD_DATA\n$realitytcp_url"
+    reality_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}-realtcp"
+    UPLOAD_DATA="$UPLOAD_DATA\n$reality_url"
+  fi
+
+  if [ -n "$SOCKS_PORT" ]; then
+    BASE64_CREDENTIALS=$(echo -n "${SOCKS_USER}:${SOCKS_PASS}" | base64)
+    socks5_url="socks://${BASE64_CREDENTIALS}@${MYIP}:${SOCKS_PORT}#${country_abbreviation}-${SUB_NAME}"
+    UPLOAD_DATA="$UPLOAD_DATA\n$socks5_url"
   fi
 
   export UPLOAD_DATA
