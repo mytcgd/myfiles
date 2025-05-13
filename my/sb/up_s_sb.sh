ebe022cf826a6b05af5916fb9ea330b697d2acc6
mytcgd <131883869+mytcgd@users.noreply.github.com>
2025-05-13 22:00:54 +0800

    Update up_s_sb.sh

--- my/sb/up_s_sb.sh
+++ my/sb/up_s_sb.sh
@@ -1,47 +1,34 @@
 #!/bin/bash
 
 general_upload_data() {
-  VMESS="{ \"v\": \"2\", \"ps\": \"${country_abbreviation}-${SUB_NAME}\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/${VMESS_WSPATH}?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\", \"fp\": \"randomized\"}"
-  vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
-  vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#${country_abbreviation}-${SUB_NAME}"
-  if [ -n "$MY_DOMAIN" ]; then
-    hysteria_url="hysteria2://${UUID}@${MY_DOMAIN}:${HY2_PORT}/?sni=www.bing.com&alpn=h3&insecure=1#${country_abbreviation}-${SUB_NAME}"
-    tuic_url="tuic://${UUID}:${tuicpass}@${MY_DOMAIN}:${TUIC_PORT}?sni=www.bing.com&congestion_control=bbr&udp_relay_mode=native&alpn=h3&allow_insecure=1#${country_abbreviation}-${SUB_NAME}"
-    reality_url="vless://${UUID}@${MY_DOMAIN}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${public_key}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}-realitytcp"
-    anytls_url="anytls://${UUID}@${MY_DOMAIN}:${ANYTLS_PORT}?insecure=1&udp=1#${country_abbreviation}-${SUB_NAME}"
-  else
-    hysteria_url="hysteria2://${UUID}@${MYIP}:${HY2_PORT}/?sni=www.bing.com&alpn=h3&insecure=1#${country_abbreviation}-${SUB_NAME}"
-    tuic_url="tuic://${UUID}:${tuicpass}@${MYIP}:${TUIC_PORT}?sni=www.bing.com&congestion_control=bbr&udp_relay_mode=native&alpn=h3&allow_insecure=1#${country_abbreviation}-${SUB_NAME}"
-    reality_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${public_key}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}-realitytcp"
-    anytls_url="anytls://${UUID}@${MYIP}:${ANYTLS_PORT}?insecure=1&udp=1#${country_abbreviation}-${SUB_NAME}"
-  fi
-
-  if [ -n "${VMESS_WSPATH}" ] && [ -z "${VLESS_WSPATH}" ]; then
-    UPLOAD_DATA="$vmess_url"
-  fi
-
-  if [ -n "${VLESS_WSPATH}" ] && [ -z "${VMESS_WSPATH}" ]; then
-    UPLOAD_DATA="$vless_url"
+  if [ -n "${V_PORT}" ]; then
+    if [ -n "${VMESS_WSPATH}" ] && [ -z "${VLESS_WSPATH}" ]; then
+      VMESS="{ \"v\": \"2\", \"ps\": \"${country_abbreviation}-${SUB_NAME}\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/${VMESS_WSPATH}?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\", \"fp\": \"randomized\"}"
+      export vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
+      UPLOAD_DATA="$vmess_url"
+    fi
+    if [ -n "${VLESS_WSPATH}" ] && [ -z "${VMESS_WSPATH}" ]; then
+      export vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#${country_abbreviation}-${SUB_NAME}"
+      UPLOAD_DATA="$vless_url"
+    fi
   fi
 
   if [ -n "$HY2_PORT" ]; then
     UPLOAD_DATA="$UPLOAD_DATA\n$hysteria_url"
   fi
-
   if [ -n "$TUIC_PORT" ]; then
     UPLOAD_DATA="$UPLOAD_DATA\n$tuic_url"
   fi
-
   if [ -n "$REAL_PORT" ]; then
     UPLOAD_DATA="$UPLOAD_DATA\n$reality_url"
   fi
-
+  if [ -n "$SOCKS_PORT" ]; then
+    UPLOAD_DATA="$UPLOAD_DATA\n$socks5_url"
+  fi
   if [ -n "$ANYTLS_PORT" ]; then
     UPLOAD_DATA="$UPLOAD_DATA\n$anytls_url"
   fi
-
   export UPLOAD_DATA
-  # echo -e "${UPLOAD_DATA}"
 }
 
 upload_url_data() {
@@ -79,7 +66,6 @@ else
 
   sleep 100
   done
-  fi
 fi
 
 # echo "upload ok!"
