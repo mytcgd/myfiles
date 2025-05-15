53f01a1de3fd3f83308081078fa58303bafb99c8
mytcgd <131883869+mytcgd@users.noreply.github.com>
2025-05-16 00:37:26 +0800

    Update up_s.sh

--- my/x/up_s.sh
+++ my/x/up_s.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 general_upload_data() {
-  # vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
-  vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#${country_abbreviation}-${SUB_NAME}"
-
-  # UPLOAD_DATA="$vmess_url\n$vless_url"
-  UPLOAD_DATA="$vless_url"
-
-  if [ -n "$REAL_PORT" ] && [ -n "$shortid" ]; then
-    realitytcp_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&sid=${shortid}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}-realtcp"
-    UPLOAD_DATA="$UPLOAD_DATA\n$realitytcp_url"
-  elif [ -n "$REAL_PORT" ] && [ -z "$shortid" ]; then
-    realitytcp_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}-realtcp"
-    UPLOAD_DATA="$UPLOAD_DATA\n$realitytcp_url"
+  if [ -n "${V_PORT}" ]; then
+    if [ -n "${VLESS_WSPATH}" ] && [ -z "${XHTTP_PATH}" ]; then
+      export vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#${country_abbreviation}-${SUB_NAME}"
+      UPLOAD_DATA="$vless_url"
+    fi
+    if [ -n "${XHTTP_PATH}" ] && [ -z "${VLESS_WSPATH}" ]; then
+      export xhttp_url="vless://${UUID}@${CF_IP}:${CFPORT}?encryption=none&security=tls&sni=${ARGO_DOMAIN}&type=xhttp&host=${ARGO_DOMAIN}&path=%2F${XHTTP_PATH}%3Fed%3D2048&mode=packet-up#${country_abbreviation}-${SUB_NAME}-xhttp"
+      UPLOAD_DATA="$xhttp_url"
+    fi
   fi
-
-  export UPLOAD_DATA
-  # echo -e "${UPLOAD_DATA}"
+  if [ -n "$REAL_PORT" ]; then
+    UPLOAD_DATA="$UPLOAD_DATA\n$reality_url"
+  fi
+  if [ -n "$SOCKS_PORT" ]; then
+    UPLOAD_DATA="$UPLOAD_DATA\n$socks5_url"
+  fi
 }
 
 upload_url_data() {
@@ -40,8 +40,6 @@ upload_url_data() {
 if [ ! -s "${FILE_PATH}/boot.log" ]; then
   general_upload_data
   upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
-elif [ -n "$MY_DOMAIN" ]; then
-  upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
 else
   while true
   do
@@ -56,7 +54,6 @@ else
 
   sleep 100
   done
-  fi
 fi
 
 # echo "upload ok!"
