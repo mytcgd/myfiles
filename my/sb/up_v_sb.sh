522737da71571964f1023f04a9b386e57383fad9
mytcgd <131883869+mytcgd@users.noreply.github.com>
2025-06-28 05:59:13 +0800

    Update up_v_sb.sh

--- my/sb/up_v_sb.sh
+++ my/sb/up_v_sb.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 if source /root/.env; then
-  upload_subscription() {
-    if command -v curl &> /dev/null; then
-      response=$(curl -s -X POST -H "Content-Type: application/json" -d "{\"URL_NAME\":\"$SUB_NAME\",\"URL\":\"$UPLOAD_DATA\"}" $SUB_URL)
-    elif command -v wget &> /dev/null; then
-      response=$(wget -qO- --post-data="{\"URL_NAME\":\"$SUB_NAME\",\"URL\":\"$UPLOAD_DATA\"}" --header="Content-Type: application/json" $SUB_URL)
-    fi
-  }
-
   export previousargoDomain=""
   while true; do
-    if [[ "$previousargoDomain" != "$ARGO_DOMAIN" ]]; then
-      upload_subscription
-      export previousargoDomain="$ARGO_DOMAIN"
-    fi
-    sleep 100
+    upload_subscription() {
+      if command -v curl &> /dev/null; then
+        response=$(curl -s -X POST -H "Content-Type: application/json" -d "{\"URL_NAME\":\"$SUB_NAME\",\"URL\":\"$UPLOAD_DATA\"}" $SUB_URL)
+      elif command -v wget &> /dev/null; then
+        response=$(wget -qO- --post-data="{\"URL_NAME\":\"$SUB_NAME\",\"URL\":\"$UPLOAD_DATA\"}" --header="Content-Type: application/json" $SUB_URL)
+      fi
+    }
+
     if [ -s "${FILE_PATH}/argo.log" ]; then
       export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
       sleep 2
@@ -54,5 +49,11 @@ if source /root/.env; then
       anytls_url="anytls://${UUID}@${MYIP}:${ANYTLS_PORT}?insecure=1&udp=1#${ISP}-${SUB_NAME}"
       UPLOAD_DATA="$UPLOAD_DATA\n$anytls_url"
     fi
+
+    if [[ "$previousargoDomain" != "$ARGO_DOMAIN" ]]; then
+      upload_subscription
+      export previousargoDomain="$ARGO_DOMAIN"
+    fi
+    sleep 100
   done
 fi
